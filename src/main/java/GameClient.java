16436979850a9216b6e04d3c2cd0a0e6d29ab9e0
YU <one445d7@gmail.com>
2020-12-16 19:25:13 +0800

    畫出封面

--- src/main/java/GameClient.java
+++ src/main/java/GameClient.java
@@ -1,6 +1,33 @@
-public class GameClient {
-    public static void main(String[] args) {
-        System.out.println("Version 1");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.ImageObserver;
+import java.awt.image.ImageProducer;
 
+public class GameClient extends JComponent {
+    private int screenWidth;
+    private int screenHeight;
+
+    GameClient() {
+        this.setPreferredSize(new Dimension(800, 600));
     }
+
+    public GameClient(int screenWidth, int screenHeight) {
+        this.screenWidth = screenWidth;
+        this.screenHeight = screenHeight;
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(new ImageIcon("assets//images//itankD.png").getImage(), 400, 100, null);
+    }
+
+
+
+
 }
+
+
+
+
